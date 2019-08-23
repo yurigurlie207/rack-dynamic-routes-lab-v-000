aa1b88fa891aeaa6ea568a7b8db3a810104de0f7
Adrienne Yao <adrienne.yao@icloud.com>
2019-08-23 02:30:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,6 @@
 class Application
 
-  @@items = [Item.new("Figs",3.42), Item.new("Pears",0.99)]
+  @@items = [Item.new("Figs", 3.42), Item.new("Pears", 0.99)]
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
