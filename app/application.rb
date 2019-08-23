744ef4e5e04cd7d52b5788db3978aaa3d1af5a38
Adrienne Yao <adrienne.yao@icloud.com>
2019-08-23 02:29:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
       item_name = req.path.split("/items/").last
       if item =@@items.find{|i| i.name == item_name}
         resp.write item.price
-      else 
+      else
         resp.status = 400
         resp.write "Item not found"
       end
